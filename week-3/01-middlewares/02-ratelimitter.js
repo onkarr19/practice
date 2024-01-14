f6f09375f4408c2747ef28a58d457bfd87d42aac
onkar <onkar.chendage@gmail.com>
2024-01-14 17:06:26 +0530

    solve middlewares

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -11,16 +11,32 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
+app.use(requestCountMiddleware)
+
+async function requestCountMiddleware(req, res, next) {
+  const userId = req.headers['user-id'];
+  if (numberOfRequestsForUser[userId] === undefined) {
+    numberOfRequestsForUser[userId] = 1;
+  } else {
+    numberOfRequestsForUser[userId]++;
+  }
+  if (numberOfRequestsForUser[userId] > 5) {
+    res.status(404).json({ msg: 'Too many requests' });
+  } else {
+    next();
+  }
+}
+
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
